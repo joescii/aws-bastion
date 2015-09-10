f3b96470cb15535c9fd48b180f62d200640ad0f2
Joe Barnes <barnesjd@gmail.com>
2015-09-10 10:05:24 -0500

    Removing the tar.gz after extraction

--- mysql.sh
+++ mysql.sh
@@ -5,4 +5,5 @@ set -e
 cd /home/ubuntu
 wget http://downloads.mysql.com/archives/get/file/mysql-5.6.19-linux-glibc2.5-x86_64.tar.gz
 gunzip -cdfv mysql-5.6.19-linux-glibc2.5-x86_64.tar.gz | tar -vx 
+rm mysql-5.6.19-linux-glibc2.5-x86_64.tar.gz
 echo "PATH=$PATH:/home/ubuntu/mysql-5.6.19-linux-glibc2.5-x86_64/bin/" >> .profile

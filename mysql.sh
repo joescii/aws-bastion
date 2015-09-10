da3b353a61deea32f6bcae042779c79625c3fdd5
Joe Barnes <barnesjd@gmail.com>
2015-09-10 16:22:18 -0500

    Updating the PATH in .bashrc for non-interactive login shells, like terraform. Also a bit of refactoring to have less copy/pasting of that long file name

--- mysql.sh
+++ mysql.sh
@@ -2,8 +2,13 @@
 
 set -e
 
+set version="5.6.19"
+set target="mysql-${version}-linux-glibc2.5-x86_64"
+set targz="${target}.tar.gz"
+
 cd /home/ubuntu
-wget http://downloads.mysql.com/archives/get/file/mysql-5.6.19-linux-glibc2.5-x86_64.tar.gz
-gunzip -cdfv mysql-5.6.19-linux-glibc2.5-x86_64.tar.gz | tar -vx 
-rm mysql-5.6.19-linux-glibc2.5-x86_64.tar.gz
-echo "PATH=\$PATH:/home/ubuntu/mysql-5.6.19-linux-glibc2.5-x86_64/bin/" >> .profile
+wget http://downloads.mysql.com/archives/get/file/${targz}
+gunzip -cdfv ${targz} | tar -vx 
+rm ${targz}
+echo "PATH=\$PATH:/home/ubuntu/${target}/bin/" >> .profile
+echo "PATH=\$PATH:/home/ubuntu/${target}/bin/" >> .bashrc
